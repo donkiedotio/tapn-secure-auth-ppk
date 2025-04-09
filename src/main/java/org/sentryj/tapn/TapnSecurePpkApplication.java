c05b0ca58ff7b210bed82db77ed5bcd97dec1be0
rajumb0232 <rajugowda0232@gmail.com>
2025-04-09 19:07:06 +0530

    project initiated

---
+++ src/main/java/org/sentryj/tapn/TapnSecurePpkApplication.java
@@ -0,0 +1,13 @@
+package org.sentryj.tapn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapnSecurePpkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapnSecurePpkApplication.class, args);
+	}
+
+}
