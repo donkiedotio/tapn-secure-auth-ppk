0ec1bbba775be939b44f0210af6b66502d27ddc5
rajumb0232 <rajugowda0232@gmail.com>
2025-04-23 18:40:20 +0530

    working on session management: created a DeviceSession class

--- src/main/java/io/donkie/tapn/secure/session/DeviceSession.java
+++ src/main/java/io/donkie/tapn/secure/session/DeviceSession.java
@@ -1,7 +1,53 @@
 package io.donkie.tapn.secure.session;
 
-public class SessionMetaData {
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+public class DeviceSession implements Serializable {
     private String sessionId;
     private String userId;
     private String deviceFingerPrint;
+    private Instant createdAt;
+
+    public static DeviceSession create(String userId, String deviceFingerPrint) {
+        DeviceSession session = new DeviceSession();
+        session.sessionId = UUID.randomUUID().toString();
+        session.userId = userId;
+        session.deviceFingerPrint = deviceFingerPrint;
+        session.createdAt = Instant.now();
+        return session;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getDeviceFingerPrint() {
+        return deviceFingerPrint;
+    }
+
+    public void setDeviceFingerPrint(String deviceFingerPrint) {
+        this.deviceFingerPrint = deviceFingerPrint;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
